acdd1c4d221180f0d980423fc4bac2a081c1b168
dongduclan270701 <dongduclan27072001@gmail.com>
2022-05-02 01:25:58 +0700

    crud_column_card

--- src/actions/ApiCall/index.js
+++ src/actions/ApiCall/index.js
@@ -2,6 +2,21 @@ import axios from 'axios'
 import { API_ROOT } from 'utils/constants'
 
 export const fetchBoardDetails = async (id) => {
-    const req = await axios.post(`${API_ROOT}/v1/boards/${id}`)
+    const req = await axios.get(`${API_ROOT}/v1/boards/${id}`)
+    return req.data
+}
+
+export const createNewColumn = async (data) => {
+    const req = await axios.post(`${API_ROOT}/v1/columns`, data)
+    return req.data
+}
+
+export const updateColumn = async (id, data) => {
+    const req = await axios.put(`${API_ROOT}/v1/columns/${id}`, data)
+    return req.data
+}
+
+export const createNewCards = async (data) => {
+    const req = await axios.post(`${API_ROOT}/v1/cards`, data)
     return req.data
 }
